ccdd55838eab4c6790c5ccd8b9bf448dfce45433
João Victor e Silva <165660227+joaovictoresilva123@users.noreply.github.com>
2025-08-18 13:17:26 -0300

    Update covid.py

--- utils/covid.py
+++ utils/covid.py
@@ -3,10 +3,7 @@ import pandas as pd
 COVID_DATA_URL = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv"
 
 def get_dados_covid() -> pd.DataFrame:
-    """
-    Carrega os dados de COVID-19 (NYTimes).
-    Retorna DataFrame com colunas: data, estado, casos, obitos
-    """
+    
     df = pd.read_csv(COVID_DATA_URL, parse_dates=["date"])
     
     df = df.rename(columns={
